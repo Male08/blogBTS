f42a7dfc762706d294df1efd9801f14a9db0e0b8
Male08 <malenafiant08@gmail.com>
2024-09-17 15:40:37 -0300

    Add files via upload

---
+++ btsjs.js
@@ -0,0 +1,11 @@
+/*const fabars=document.querySelector('.fa-bars');
+const menuhorizontal=document.querySelector('.menu-horizontal');
+const menuvertical=document.querySelector('menu-vertical');
+fabars.addEventListener("click",()=>{
+    menuhorizontal.classList.toggle('clasemenu');
+} ) ;
+menuvertical.foreach(function(item){
+    item.addEventListener('click', function(){
+        menuhorizontal.classList.remove('clasemenu');
+    });
+});*/
